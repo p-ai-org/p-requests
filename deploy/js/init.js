3bcb86adefc4d6bdaba80eeda6b1954e76a222b7
Hana Ahmed <59346862+hahmed17@users.noreply.github.com>
2020-12-04 21:18:19 -0800

    Added collapsible functionality

--- deploy/js/init.js
+++ deploy/js/init.js
@@ -4,6 +4,11 @@
     $('.sidenav').sidenav();
     $('.parallax').parallax();
     $(".dropdown-trigger").dropdown();
+    $(document).ready(function(){
+      $('.collapsible').collapsible();
+    });
+
+
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
