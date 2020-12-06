1ec7d71722772bff6de59a207e9a05c419aca3d9
ngetachew <natugetachew@gmail.com>
2020-12-05 16:18:48 -0800

    Fixed pipeline to work on website

--- 311_Requests_Model/EDPipeline.py
+++ 311_Requests_Model/EDPipeline.py
@@ -16,15 +16,15 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn import tree
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
-#Other model will go here as well
 import LACER as lc
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import mean_absolute_error
 from sgcrf import SparseGaussianCRF
 import pickle
+from joblib import dump, load
 
 #Features to use in our model, c = categorical, d = numeric
-c = ['AssignTo', 'RequestType', 'RequestSource', 'Month', 'Anonymous', 'CreatedByUserOrganization','CD']
+c = ['AssignTo', 'RequestType', 'RequestSource', 'Month', 'Anonymous', 'CreatedByUserOrganization']
 d = ['Latitude', 'Longitude']
     
 #Slightly editied lacer funcions
@@ -96,34 +96,46 @@ def gelev(val):
 '''
 Preprocessing function. Takes in the file path to the data and loads it in a DataFrame, then calcuates the elapsed days per request and marks them as more than or less than eleven days. Then it encodes the appropriate values and returns the train data, labels, and the formatted dataframe.
 '''
-def preprocess(df, formatted=False,encode=True):
+def preprocess(df, formatted=False,encode=False):
     if not formatted:
         df['Just Date'] = df['Just Date'].apply(lambda x: datetime.strptime(x,'%Y-%m-%d'))
     df['Eleven'] = df['ElapsedDays'].apply(gelev, 0)
+    df['Month'] = df['Just Date'].dt.month_name()
     #Put desired columns into dataframe, drop nulls. 
-    dfn = df.filter(items = c + d + ['ElapsedDays','Just Date','CreatedDate','ClosedDate'])
+    dfn = df.filter(items = c + d + ['ElapsedDays','Just Date','CreatedDate','ClosedDate','CD'])
+    dfn = dfn.dropna()
+    #Separate data into explanatory and response variables
+    XCAT = dfn.filter(items = c).values
+    XNUM = dfn.filter(items = d).values
+    y = dfn['ElapsedDays'] <= 11
+    #Encode cateogrical data and merge with numerical data
+    if encode:
+        onehotencoder = OneHotEncoder()
+        print(XCAT.shape)
+        onehotencoder.fit_transform(XCAT)
+        #XCAT = onehotencoder.fit_transform(XCAT).toarray()
+        X = np.concatenate((XCAT, XNUM), axis=1)
+        print()
+        dump(onehotencoder,'onehot.joblib')
+        #pickle.dump(onehotencoder, open("encoder.pkl", "wb"))
+    else:
+        onehotencoder = load('onehot.joblib')
+        XCAT = onehotencoder.transform(XCAT).toarray()
+        X = np.concatenate((XCAT, XNUM), axis=1)
+    return X,y, dfn
+
+def preprocess_request(df):
+    dfn = df.filter(items = c + d)
     dfn = dfn.dropna()
     #Separate data into explanatory and response variables
     XCAT = dfn.filter(items = c).values
     XNUM = dfn.filter(items = d).values
     y = None
     #Encode cateogrical data and merge with numerical data
-    if encode:
-        onehotencoder = OneHotEncoder()
-        y = dfn['ElapsedDays'] <= 11
-        #Dump encoder class data to pickle
-        #labelencoder_X = LabelEncoder()
-        #for num in range(len(c)): 
-        #    XCAT[:, num] = labelencoder_X.fit_transform(XCAT[:, num])
-        print(XCAT.shape)
-        XCAT = onehotencoder.fit_transform(XCAT).toarray()
-        X = np.concatenate((XCAT, XNUM), axis=1)
-        pickle.dump(onehotencoder, open("encoder.pickl", "wb"))
-    else:
-        onehotencoder = pickle.load('encoder.pkl')
-        XCAT = onehotencoder.fit_transform(XCAT).toarray()
-        X = np.concatenate((XCAT, XNUM), axis=1)
-    return X,y, dfn
+    onehotencoder = load('onehot.joblib')
+    XCAT = onehotencoder.transform(XCAT).toarray()
+    X = np.concatenate((XCAT, XNUM), axis=1)
+    return X, dfn
 
 '''
 Runs the model that classifies each request as more than or less than/equal to 11 days. Parameters are the hyperparameters for the model itself, and the train data and labels.
@@ -161,13 +173,13 @@ def split_to_models(df,formatted=False, encode=True):
     X, y, df = preprocess(df,formatted)
     print('Creating 11 day classifier')
     #Train on 3 yrs before data
+    print(X.shape)
     model_eleven = estimation_model(50,20,X,y)
     df['LessEqualEleven'] = model_eleven.predict(X)
     df['LessEqualEleven'] = df['LessEqualEleven'].apply(lambda x: int(x))
     df_sgcrf = df[df['LessEqualEleven'] == 1.0]
     df_other = df[df['LessEqualEleven'] == 0.0]
     return df_sgcrf, df_other
-
 '''
 Takes the data file path and parameters for the lacer model and runs the pipeline to get the appropriate dataframes, then runs both models. This is the main function you should run from this file.
 '''
