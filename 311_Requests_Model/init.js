bf2274b06679a3c2bf4292a65589c9e55b96ee05
Hana Ahmed <59346862+hahmed17@users.noreply.github.com>
2020-12-06 10:11:14 -0800

    added css and js files

---
+++ 311_Requests_Model/init.js
@@ -0,0 +1,15 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+    $(".dropdown-trigger").dropdown();
+    $(document).ready(function(){
+      $('.collapsible').collapsible();
+    });
+    
+
+
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
